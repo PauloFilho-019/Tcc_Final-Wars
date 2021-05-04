bfbbb30a3338f5661d90f38e5d7f034415bdb176
Leonardo Silva <leonardo.silva@jaguar.ind.br>
2021-05-04 16:46:42 -0300

    atualizando página suporte

--- Projetão/TCC_SAUDE/SISTEMA_WEB/suporte/view/suporte.js
+++ Projetão/TCC_SAUDE/SISTEMA_WEB/suporte/view/suporte.js
@@ -1,9 +1,1 @@
-function imageClick(){
-    "use strict";
-  document.getElementById('image').src = " "; document.getElementById('imageClick').innerHTML = " ";
-  document.getElementById('footer').style.color = "white";  
-  }
-  function Thanks(){
-   document.getElementById('Thanks').innerHTML = "Thank you!";
-    } 
-  
+const projectName = 'survey-form';
